9a8fee48710f1acca9360aff6bdd791eaece122e
Fagner Brack <github@fagnermartins.com>
2013-04-29 12:24:43 -0300

    Melhora os botões do jqueryUI
    Closes gh-41.

--- styles/tests/tests.js
+++ styles/tests/tests.js
@@ -5,5 +5,11 @@
 			this.title = "";
 			return "<div class='title'>" + title + ":</div>";
 		});
+		
+		// Instancia os plugins jQuery UI
+		$("[data-widget]").each(function() {
+			var data = $( this ).data();
+			$( this )[ data.widget ]( data );
+		});
 	});
 }());
