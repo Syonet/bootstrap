424499a995a6d90d667fe75f6d059f4f012acb90
Gustavo Henke <gustavo@injoin.com.br>
2013-05-15 14:53:36 -0300

    Conversão do syoDataGrid para API jQuery UI Closes gh-79, gh-72

--- scripts/tests/specs/datagrid/classes.js
+++ scripts/tests/specs/datagrid/classes.js
@@ -1,81 +1,78 @@
 (function() {
 	"use strict";
-	
+
 	module( "Classes", {
 		setup: function() {
 			$( "#grid-head-body, #grid-full" ).syoDataGrid();
 		}
 	});
-	test( "Classes padrões - sem evento", function() {
-		expect( 9 );
-		
+	test( "Classes padrão - sem evento", function() {
+		expect( 4 );
+
 		var $el = $( "#grid-head-body" );
-		var $trs = $el.children( "div:last" ).find( "tr" );
-		
-		$trs.each(function() {
-			ok( $( this ).hasClass( "syo-datagrid-state-default" ), "All TRs should have default state" );
-		});
-		
+
 		ok( $el.hasClass( "syo-datagrid" ), "Classe padrão do container principal" );
 		ok( $el.children( "div:eq(0)" ).hasClass( "syo-datagrid-header" ), "Classe padrão do header" );
 		ok( $el.children( "div:eq(1)" ).hasClass( "syo-datagrid-body" ), "Classe padrão do body" );
-		ok( $el.find( "div:eq(1) > div:first" ).hasClass( "syo-datagrid-rowcont" ),
-				"Classe padrão do container das linhas" );
+		ok(
+			$el.find( "div:eq(1) > div:first" ).hasClass( "syo-datagrid-rowcont" ),
+			"Classe padrão do container das linhas"
+		);
 	});
-	test( "Classes padrões - evento:mouseenter", function() {
-		expect( 5 );
-		
+	test( "Classes padrão - evento: mouseenter", function() {
+		expect( 2 );
+
 		var $el = $( "#grid-head-body" );
 		var $trs = $el.children( "div:last" ).find( "tr" );
 		var $tr = $trs.slice( 0, 1 );
-		
+		var testeSiblings = true;
+
 		$tr.trigger( "mouseenter" );
-		ok( $tr.hasClass( "syo-datagrid-state-hover" ), "Classe hover" );
+		ok( $tr.hasClass( "syo-datagrid-state-hover" ), "Classe do estado hover" );
 		$tr.siblings().each(function() {
-			ok( $( this ).hasClass( "syo-datagrid-state-default" ),
-					"Todos os elementos siblings devem possuir a classe padrão" );
+			testeSiblings = testeSiblings && !$( this ).hasClass( "syo-datagrid-state-hover" );
 		});
-		
+
+		ok( testeSiblings, "Todos os siblings não devem possuir a classe do estado hover" );
 	});
-	test( "Classes padrões - evento:mouseleave", function() {
-		expect( 5 );
-		
+	test( "Classes padrão - evento: mouseleave", function() {
+		expect( 1 );
+
 		var $el = $( "#grid-head-body" );
 		var $trs = $el.children( "div:last" ).find( "tr" );
 		var $tr = $trs.slice( 0, 1 );
-		
+
 		$tr.trigger( "mouseleave" );
-		ok( $tr.hasClass( "syo-datagrid-state-default" ), "Classe padrão");
-		$tr.siblings().each(function() {
-			ok( $( this ).hasClass( "syo-datagrid-state-default" ), "Todos os siblings devem possuir a classe padrão" );
-		});
+		ok( !$tr.hasClass( "syo-datagrid-state-hover" ), "Sem classe do estado hover");
 	});
 	test( "Não deve escutar os eventos em trs dentro do tfoot, ele não deve ser usado para manipulação", function() {
 		expect( 2 );
-		
+
 		var hoverClass = "syo-datagrid-state-hover";
 		var $bodyTR = $( "#grid-full tbody tr:first" );
 		var $footerTR = $( "#grid-full tfoot tr:first" );
-		
+
 		$bodyTR.trigger( "mouseenter" );
 		ok( $bodyTR.hasClass( hoverClass ), "Confirma que a classe padrão está funcionando para testar lá embaixo" );
-		
+
 		$footerTR.trigger( "mouseenter" );
 		ok( !$footerTR.hasClass( hoverClass ) );
-		
+
 	});
-	test( "Classes padrões - evento:click", function() {
-		expect( 5 );
-		
+	test( "Classes padrões - evento: click", function() {
+		expect( 2 );
+
 		var $el = $( "#grid-head-body" );
 		var $trs = $el.children( "div:last" ).find( "tr" );
 		var $tr = $trs.slice( 0, 1 );
-		
+		var testeSiblings = true;
+
 		$tr.trigger( "click" );
-		ok( $tr.hasClass( "syo-datagrid-state-clicked" ), "Classe de estado de click" );
+		ok( $tr.hasClass( "syo-datagrid-state-clicked" ), "Classe do estado clicked" );
 		$tr.siblings().each(function() {
-			ok( $( this ).hasClass( "syo-datagrid-state-default" ),
-					"Todos os siblings devem possuir a classe padrãos" );
+			testeSiblings = testeSiblings && !$( this ).hasClass( "syo-datagrid-state-clicked" );
 		});
+
+		ok( testeSiblings, "Todos os siblings não devem possuir a classe do estado clicked" );
 	});
 }());
--- scripts/tests/specs/datagrid/events.js
+++ scripts/tests/specs/datagrid/events.js
@@ -1,65 +1,110 @@
 (function() {
 	"use strict";
-	
+
 	function multiplyTestContainerBy( times ) {
 		var fixtureContent = "";
 		var i = 0;
 		var $fixture = $( "#qunit-fixture" );
 		var HTML = $fixture.html();
-		
+
 		$fixture.empty();
-		
+
 		while ( ++i <= times ) {
 			fixtureContent += HTML.replace( "id=\"grid-head-body\"", "id=\"grid-" + i + "\"" );
 		}
-		
+
 		$fixture.append( fixtureContent );
 	}
-	
+
 	module( "Eventos" );
-	test( "click", function() {
-		expect( 3 );
-		
-		var $tr;
-		var count = 0;
+
+	test( "beforeActivate", function() {
+		expect( 4 );
+
+		var $tr, eventArgs;
 		var triggers = [];
-		
+
 		$( "#grid-head-body" ).syoDataGrid({
-			click: function( event ) {
-				var context = this;
-				
-				triggers[ count++ ] = {
-					context: context,
-					args: [ event ]
-				};
+			beforeActivate: function( event, data ) {
+				triggers.push({
+					context: this,
+					args: [ event, data ]
+				});
 			}
 		});
-		
+
 		$tr = $( "#grid-head-body div:last tr:eq(0)" );
-		
+
 		$tr.trigger("click");
 		strictEqual( triggers.length, 1, "Function should be triggered on tr click" );
-		strictEqual( triggers[ 0 ].context, $tr[ 0 ], "The context should be a DOMElement" );
-		ok( triggers[ 0 ].args[ 0 ] instanceof $.Event, "Should pass the jQery Event Object as the first argument" );
+
+		eventArgs = triggers[ 0 ].args;
+		ok( eventArgs[ 0 ] instanceof $.Event, "Should pass the jQuery Event Object as the first argument" );
+		ok(
+			eventArgs[ 1 ].oldItem instanceof $,
+			"Should pass the old active jQuery Element in the second argument"
+		);
+		ok(
+			eventArgs[ 1 ].newItem instanceof $,
+			"Should pass the old active jQuery Element in the second argument"
+		);
 	});
-	test( "create", function() {
-		expect( 3 );
-		
-		var count = 0;
+
+	test( "activate", function() {
+		expect( 4 );
+
+		var $tr, eventArgs;
 		var triggers = [];
-		
-		multiplyTestContainerBy( 2 );
-		
-		$( "#grid-1, #grid-2" ).syoDataGrid({
-			create: function() {
-				triggers[ count++ ] = this;
+
+		$( "#grid-head-body" ).syoDataGrid({
+			activate: function( event, data ) {
+				triggers.push({
+					context: this,
+					args: [ event, data ]
+				});
 			}
 		});
-		
+
+		$tr = $( "#grid-head-body div:last tr:eq(0)" );
+
+		$tr.trigger("click");
+		strictEqual( triggers.length, 1, "Function should be triggered on tr click" );
+
+		eventArgs = triggers[ 0 ].args;
+		ok( eventArgs[ 0 ] instanceof $.Event, "Should pass the jQuery Event Object as the first argument" );
+		ok(
+			eventArgs[ 1 ].oldItem instanceof $,
+			"Should pass the old active jQuery Element in the second argument"
+		);
+		ok(
+			eventArgs[ 1 ].newItem instanceof $,
+			"Should pass the old active jQuery Element in the second argument"
+		);
+	});
+
+	test( "create", function() {
+		expect( 3 );
+
+		var triggers = [];
+
+		multiplyTestContainerBy( 2 );
+
+		$( "#grid-1, #grid-2" ).syoDataGrid({
+			create: function() {
+				triggers.push( this );
+			}
+		});
+
 		strictEqual( triggers.length, 2, "The create event has been triggered" );
-		strictEqual( triggers[ 0 ], $( "#grid-1" )[ 0 ],
-				"The context of the first element should be correct DOMElement" );
-		strictEqual( triggers[ 1 ], $( "#grid-2" )[ 0 ],
-				"The context of the second element should be correct DOMElement" );
+		strictEqual(
+			triggers[ 0 ],
+			$( "#grid-1" )[ 0 ],
+			"The context of the first element should be correct DOMElement"
+		);
+		strictEqual(
+			triggers[ 1 ],
+			$( "#grid-2" )[ 0 ],
+			"The context of the second element should be correct DOMElement"
+		);
 	});
 }());
