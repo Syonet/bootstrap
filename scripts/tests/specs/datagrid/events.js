caf8c966c3158933e00cb6e9c083e127c08c2806
Gustavo Henke <gustavo@injoin.com.br>
2013-05-15 11:15:04 -0300

    Refatoração do módulo QUnit 'Eventos'

--- scripts/tests/specs/datagrid/events.js
+++ scripts/tests/specs/datagrid/events.js
@@ -1,65 +1,58 @@
 (function() {
 	"use strict";
-	
+
 	function multiplyTestContainerBy( times ) {
 		var fixtureContent = "";
 		var i = 0;
 		var $fixture = $( "#qunit-fixture" );
 		var HTML = $fixture.html();
-		
+
 		$fixture.empty();
-		
+
 		while ( ++i <= times ) {
 			fixtureContent += HTML.replace( "id=\"grid-head-body\"", "id=\"grid-" + i + "\"" );
 		}
-		
+
 		$fixture.append( fixtureContent );
 	}
-	
+
 	module( "Eventos" );
 	test( "click", function() {
-		expect( 3 );
-		
+		expect( 2 );
+
 		var $tr;
-		var count = 0;
 		var triggers = [];
-		
+
 		$( "#grid-head-body" ).syoDataGrid({
-			click: function( event ) {
-				var context = this;
-				
-				triggers[ count++ ] = {
-					context: context,
+			activate: function( event ) {
+				triggers.push({
+					context: this,
 					args: [ event ]
-				};
+				});
 			}
 		});
-		
+
 		$tr = $( "#grid-head-body div:last tr:eq(0)" );
-		
+
 		$tr.trigger("click");
 		strictEqual( triggers.length, 1, "Function should be triggered on tr click" );
-		strictEqual( triggers[ 0 ].context, $tr[ 0 ], "The context should be a DOMElement" );
-		ok( triggers[ 0 ].args[ 0 ] instanceof $.Event, "Should pass the jQery Event Object as the first argument" );
+		ok( triggers[ 0 ].args[ 0 ] instanceof $.Event, "Should pass the jQuery Event Object as the first argument" );
 	});
 	test( "create", function() {
 		expect( 3 );
-		
-		var count = 0;
+
 		var triggers = [];
-		
+
 		multiplyTestContainerBy( 2 );
-		
+
 		$( "#grid-1, #grid-2" ).syoDataGrid({
 			create: function() {
-				triggers[ count++ ] = this;
+				triggers.push( this );
 			}
 		});
-		
+
 		strictEqual( triggers.length, 2, "The create event has been triggered" );
-		strictEqual( triggers[ 0 ], $( "#grid-1" )[ 0 ],
-				"The context of the first element should be correct DOMElement" );
-		strictEqual( triggers[ 1 ], $( "#grid-2" )[ 0 ],
-				"The context of the second element should be correct DOMElement" );
+		strictEqual( triggers[ 0 ], $( "#grid-1" )[ 0 ], "The context of the first element should be correct DOMElement" );
+		strictEqual( triggers[ 1 ], $( "#grid-2" )[ 0 ], "The context of the second element should be correct DOMElement" );
 	});
 }());
